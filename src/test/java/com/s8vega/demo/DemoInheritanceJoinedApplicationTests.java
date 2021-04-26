42c1bc92531b1698cc211f18e6ad4c6667f55b20
S8Vega <bsebastianvg18@gmail.com>
2021-04-26 12:30:58 -0500

    Initial commit

---
+++ src/test/java/com/s8vega/demo/DemoInheritanceJoinedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s8vega.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInheritanceJoinedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

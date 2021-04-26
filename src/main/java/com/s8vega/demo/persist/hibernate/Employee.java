dbe477952f575327dfa32f1978f37ca157f06b64
S8Vega <bsebastianvg18@gmail.com>
2021-04-26 13:04:15 -0500

    add employee class

---
+++ src/main/java/com/s8vega/demo/persist/hibernate/Employee.java
@@ -0,0 +1,30 @@
+package com.s8vega.demo.persist.hibernate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Employee")
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Employee implements java.io.Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nif;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+}
